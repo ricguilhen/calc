164b87699ba12cf3c8ed37e02a3d4056d96b87dd
unknown <rguilhenmelo@gmail.com>
2026-03-03 20:25:32 -0300

    Todas as funções adicionadas (back-end)

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,66 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import 'react-native-reanimated';
+import { View } from 'react-native-reanimated/lib/typescript/Animated';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
-import { useColorScheme } from '@/hooks/use-color-scheme';
 
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
+  
+const [currValue, setCurrValue] = useState('0')
+const [lastValue, setLastValue] = useState('')
+const [operator, setOperator] = useState('')
+const [result, setResult] = useState('')
+
+const handleNum = (num: string) => {
+  if (currValue === '0') setCurrValue(num)
+  else setCurrValue(prev => prev + num)
+}
+
+const handleOperator = (op: string) => {
+  setLastValue(currValue)
+  setOperator(op)
+  setCurrValue('0')
+}
+
+const handleIgual = () => {
+  const a = parseFloat(lastValue)
+  const b = parseFloat(currValue)
+  let res = 0
+
+  if (operator === '+') res = a + b
+  if (operator === '-') res = a - b
+  if (operator === '*') res = a * b
+  if (operator === '/') res = a / b
+
+  setResult(String(res))
+  setCurrValue(String(res))
+  setLastValue('')
+  setOperator('')
+}
+
+const handleSqrt = () => {
+  const n = parseFloat(currValue)
+  const res = n >= 0 ? Math.sqrt(n) : 'Erro'
+  setCurrValue(String(res))
+  setResult(String(res))
+}
+
+const handleReset = () => {
+  setCurrValue('0')
+  setLastValue('')
+  setOperator('')
+}
+
+const displayExpressao = lastValue
+? `${lastValue} ${operator} ${currValue}`
+  : currValue
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <View>
+        <SafeAreaView>
+
+        </SafeAreaView>
+    </View>
   );
 }
